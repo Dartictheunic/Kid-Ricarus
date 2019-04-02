166453384392b6f6ee8b8d6aede86447625bb5c0
Louis <houyez.louis.lille@gmail.com>
2019-04-02 16:40:15 +0200

    CourantAerien Bon ( à tweeker)

---
+++ Kid Ricarus/Assets/GD/Louis/SCRIPT/ModuleLD/CercleAura.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aura : MonoBehaviour
+{
+    public PlayerController playerScript;
+
+    public AnimationCurve boostCurve;
+    public float multiplier;
+
+    float baseForwardSpeed;
+    float curveTime = 4f;
+
+    // Update is called once per frame 
+    void FixedUpdate()
+    {
+        if (curveTime < 3.0f)
+        {
+            applyBoostAura();
+        }
+    }
+
+    public void applyBoostAura()
+    {
+        curveTime += Time.deltaTime;
+        float curveAmount = boostCurve.Evaluate(curveTime) * baseForwardSpeed;
+        playerScript.forwardSpeed = curveAmount;
+        Debug.Log(playerScript.forwardSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        startBoostAura();
+    }
+
+    public void startBoostAura()
+    {
+        baseForwardSpeed = playerScript.forwardSpeed;
+        curveTime = 0f;
+        Debug.Log("oui ca marche");
+    }
+}
