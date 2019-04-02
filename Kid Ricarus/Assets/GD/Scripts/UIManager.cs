a995a00a0844195e36aeec79ea2c00b2f813d803
Alexis <dartic.axj@gmail.com>
2019-04-02 20:31:35 +0200

    J'crois pas avoir cassé le truc

---
+++ Kid Ricarus/Assets/GD/Scripts/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager uimanager;
+    public GameObject[] menus;
+    public GameObject baseMenu;
+
+    bool isGamePaused;
+
+    private void Awake()
+    {
+        uimanager = this;
+    }
+
+    public void ActivateMenuItem(GameObject menuToActivate)
+    {
+        foreach(GameObject go in menus)
+        {
+            if(go == menuToActivate)
+            {
+                if (go.activeInHierarchy)
+                {
+                    go.SetActive(false);
+                }
+
+                else
+                {
+                    go.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void SettingsButtonPushed()
+    {
+        if(!isGamePaused)
+        {
+            isGamePaused = true;
+            baseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+        else
+        {
+            isGamePaused = false;
+            baseMenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+}
